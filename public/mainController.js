7b52d955658bf4b2d745d5669e6d6f8eec021687
benjkn <benj.kn@gmail.com>
2017-02-26 13:59:07 +0200

    2

--- public/mainController.js
+++ public/mainController.js
@@ -21,15 +21,44 @@ app.controller("mainCtrl", ['$scope', function($scope) {
 	$scope.numberOfSuitcases = 36;
 	$scope.costOfSuitcase = 45;
 
+
+
 	$scope.update = function () {
-		$scope.perPart = Math.round(($scope.others+$scope.ticketsCost+(($scope.participants-$scope.free)*425))/($scope.participants-$scope.arxigoi))
+		$scope.total = (($scope.numberOfSuitcases*$scope.costOfSuitcase)+$scope.others+$scope.ticketsCost+(($scope.participants-$scope.free)*425))
+		$scope.perPart = Math.round($scope.total/($scope.participants-$scope.arxigoi))
 	};
 
 	$scope.update();
 
 	$scope.updateNew = function () {
-		$scope.perPartNew = Math.round(($scope.others+$scope.ticketsCost-($scope.adults*$scope.adultsPay)+(($scope.participants-$scope.free)*425))/($scope.participants-$scope.arxigoi-$scope.adults))
+		$scope.totalNew =  $scope.total
+		$scope.ypoloipoi = $scope.participants - $scope.arxigoi
+
+		for (i=0; i<$scope.indivs.length; i++) {
+			$scope.totalNew -= $scope.indivs[i].people*$scope.indivs[i].amount
+			$scope.ypoloipoi -= $scope.indivs[i].people
+		}
+
+		$scope.perPartNew = Math.round($scope.totalNew/$scope.ypoloipoi)
 	};
+
+	$scope.indivs = [
+		{
+			people: 3,
+			amount: 750
+		}
+	]
+
+	$scope.addNew = function () {
+		$scope.indivs.push({people:0, amount:0})
+		$scope.updateNew()
+	}
+
+	$scope.edit = function (trackNo, what) {
+		var editable = prompt("Enter new number for" + what + ":")
+		$scope.indivs[trackNo][what] = editable
+		$scope.updateNew()
+	}
 
 	$scope.updateNew();
 
