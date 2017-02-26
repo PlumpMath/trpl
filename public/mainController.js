8db01c6967557dac7e735f7efa53519ca8801c55
benjkn <benj.kn@gmail.com>
2017-02-26 17:19:34 +0200

    3

--- public/mainController.js
+++ public/mainController.js
@@ -10,10 +10,12 @@ app.controller("mainCtrl", ['$scope', function($scope) {
 		}
 	}
 
+	$scope.test = false;
+	$scope.test2 = false;
 	$scope.free = 20;
 	$scope.participants = 51;
 	$scope.ticketsCost = 14075;
-	$scope.others = 1400;
+	$scope.others = 1450;
 	$scope.arxigoi = 4;
 	$scope.cost = 425;
 	$scope.adults = 3;
@@ -24,6 +26,8 @@ app.controller("mainCtrl", ['$scope', function($scope) {
 
 
 	$scope.update = function () {
+		$scope.fees = ($scope.participants - $scope.free)*$scope.cost
+		$scope.suitcases = $scope.numberOfSuitcases*$scope.costOfSuitcase
 		$scope.total = (($scope.numberOfSuitcases*$scope.costOfSuitcase)+$scope.others+$scope.ticketsCost+(($scope.participants-$scope.free)*425))
 		$scope.perPart = Math.round($scope.total/($scope.participants-$scope.arxigoi))
 	};
@@ -44,19 +48,42 @@ app.controller("mainCtrl", ['$scope', function($scope) {
 
 	$scope.indivs = [
 		{
-			people: 3,
+			people: 6,
 			amount: 750
+		},
+		{
+			people: 1,
+			amount: 600
+		},
+		{
+			people: 1,
+			amount: 350
+		},
+		{
+			people: 2,
+			amount: 220
 		}
 	]
 
 	$scope.addNew = function () {
 		$scope.indivs.push({people:0, amount:0})
+		if ($scope.indivs.length == 1) {
+			$scope.test2  = false;
+		}
 		$scope.updateNew()
 	}
 
 	$scope.edit = function (trackNo, what) {
-		var editable = prompt("Enter new number for" + what + ":")
+		var editable = prompt("Enter new number for " + what + ":")
 		$scope.indivs[trackNo][what] = editable
+		$scope.updateNew()
+	}
+
+	$scope.removeLine = function (index) {
+		$scope.indivs.splice(index, 1)
+		if ($scope.indivs.length == 0) {
+			$scope.test2 = true
+		}
 		$scope.updateNew()
 	}
 
